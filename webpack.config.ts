9b213738c1d3aa939bc921a93855d6f41d2734ac
b.omirzak <b.omirzak@documentolog.com>
2023-07-09 17:05:41 +0600

    3000 fix webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,13 +2,21 @@ import * as path from 'path';
 import * as webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import StatoscopePlugin from '@statoscope/webpack-plugin';
+import fs from 'fs';
 
+
+const pagesDir = './src/pages/';
+const entryFiles = fs.readdirSync(path.resolve(__dirname, pagesDir))
+  .filter(file => path.extname(file) === '.tsx')
+  .reduce<{ [key: string]: string }>((entries, file) => {
+    const fileName = path.parse(file).name;
+    entries[fileName] = path.resolve(__dirname, pagesDir, file);
+    return entries;
+  }, {});
+  
 const config: webpack.Configuration = {
     mode: 'production',
-    entry: {
-        main: './src/pages/root.tsx',
-        secondary: './src/pages/root2.tsx'
-    },
+    entry: entryFiles,
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js',
@@ -30,10 +38,10 @@ const config: webpack.Configuration = {
     module: {
         rules: [
             {
-                test: /\.tsx?$/, // updated to include '.tsx' files
+                test: /\.tsx?$/,
                 use: [
                     {
-                        loader: 'ts-loader', // added 'ts-loader'
+                        loader: 'ts-loader',
                     },
                     {
                         loader: path.resolve(__dirname, 'loaders/i18n-loader.cjs'),
